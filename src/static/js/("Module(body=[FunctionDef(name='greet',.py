f8e52bf7d0ee32491d5352c3db0c4cb44374e35f
parveennaaz-012908 <37070912+parveennaaz-012908@users.noreply.github.com>
2024-06-04 14:13:22 +0200

    all files

---
+++ src/static/js/("Module(body=[FunctionDef(name='greet',.py
@@ -0,0 +1,7 @@
+("Module(body=[FunctionDef(name='greet', args=arguments(posonlyargs=[], "
+ "args=[arg(arg='name')], kwonlyargs=[], kw_defaults=[], defaults=[]), "
+ "body=[Expr(value=Call(func=Name(id='print', ctx=Load()), "
+ "args=[BinOp(left=BinOp(left=Constant(value='Hello, '), op=Add(), "
+ "right=Name(id='name', ctx=Load())), op=Add(), right=Constant(value='!'))], "
+ "keywords=[]))], decorator_list=[]), Expr(value=Call(func=Name(id='greet', "
+ "ctx=Load()), args=[Constant(value='John')], keywords=[]))], type_ignores=[])")
